e2b41bc4e9ccbd14b3095a5c105d8fb00f63803e
briankwong65 <briankwong98@gmail.com>
2021-04-20 19:09:01 +0800

    Update Node styles

--- src/components/Node/index.js
+++ src/components/Node/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import * as styles from './styles.module.scss';
-import classNames from 'classnames';
 
 const Node = (props) => {
-  const { name, weight, value } = props;
-  const roundedValue = value.toFixed(2);
-  return roundedValue >= 0 ? (
-    <div className={styles.green}>
+  const { name, width, totalWidth, value } = props;
+  const percentageValue = (value*100).toFixed(2);
+  const color = percentageValue >= 0 ? 'green' : 'red';
+  return( 
+    <div className={styles.node} style={{backgroundColor: color, width: width*totalWidth*100}}>
       {name}
       <br />
-      {roundedValue}
+      {percentageValue}%
     </div>
-  ) : (
-    <div className={styles.red}>
-      {name}
-      <br />
-      {roundedValue}
-    </div>
-  );
+  )
 };
 
 export default Node;
