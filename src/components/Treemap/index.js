7d0cebcff34f3c0206fdf4da154de7fb05ef1f19
briankwong65 <briankwong98@gmail.com>
2021-04-20 20:18:03 +0800

    Handle row >= number of data

--- src/components/Treemap/index.js
+++ src/components/Treemap/index.js
@@ -14,50 +14,58 @@ const Treemap = (props) => {
     const sortedDataList = JSON.parse(JSON.stringify(dataList)).sort((a, b) => {
       return b.weight - a.weight;
     });
-    const weightSum = sortedDataList.reduce((sum, cur) => sum + cur.weight, 0);
-    const width = Math.ceil(weightSum / row);
-    let currentRow = 0;
-    let remainingWidth = width;
-    console.log('dataList', sortedDataList);
-    console.log('row', row);
-    const dataListTemp = [...sortedDataList];
-    const formattedDataTemp = Array.from({length: row}, () => []);
-    while (currentRow < row) {
-      let i = 0;
-      while (dataListTemp.length > 0 && i < dataListTemp.length){
-        if (dataListTemp[i].weight <= remainingWidth){
-          // console.log('currentRow', currentRow);
-          // console.log("remainingWidth", remainingWidth );
-          // console.log("weight", dataListTemp[i].weight);
-          remainingWidth -= dataListTemp[i].weight;
-          formattedDataTemp[currentRow].push(dataListTemp[i]);
-          dataListTemp.splice(i, 1);
-          i = 0;   
-          if (remainingWidth === 0) {
-            currentRow += 1;
-            remainingWidth = width;
-          }       
-        }
-        else {
-          i += 1;
-        }
-      }
-      currentRow += 1;
-      remainingWidth = width;
-    }
 
-    setWeightSum(weightSum);
-    setWidth(width);
-    setFormattedData(formattedDataTemp);
-    console.log('formatted data', formattedDataTemp);
+    if (row >= sortedDataList.length) {
+      const formattedDataTemp = Array.from({length: row}, () => []);
+      sortedDataList.forEach((data, index) => {
+        formattedDataTemp[index].push(data);
+      })
+      setFormattedData(formattedDataTemp);
+    }
+    else{
+      const weightSum = sortedDataList.reduce((sum, cur) => sum + cur.weight, 0);
+      const width = Math.ceil(weightSum / row);
+      let currentRow = 0;
+      let remainingWidth = width;
+      console.log('dataList', sortedDataList);
+      console.log('row', row);
+      const dataListTemp = [...sortedDataList];
+      const formattedDataTemp = Array.from({length: row}, () => []);
+      
+      while (currentRow < row) {
+        let i = 0;
+        while (dataListTemp.length > 0 && i < dataListTemp.length){
+          if (dataListTemp[i].weight <= remainingWidth){
+            remainingWidth -= dataListTemp[i].weight;
+            formattedDataTemp[currentRow].push(dataListTemp[i]);
+            dataListTemp.splice(i, 1);
+            i = 0;   
+            if (remainingWidth === 0) {
+              currentRow += 1;
+              remainingWidth = width;
+            }       
+          }
+          else {
+            i += 1;
+          }
+        }
+        currentRow += 1;
+        remainingWidth = width;
+      }
+
+      setWeightSum(weightSum);
+      setWidth(width);
+      setFormattedData(formattedDataTemp);
+      console.log('formatted data', formattedDataTemp);
+    }
   }, [dataList, row]);
   
   return (
-    <div className={classNames(styles.container, className)}>
+    <div className={classNames(styles.treemap, className)}>
       {
         formattedData.map((dataRow, row) => {
           console.log('dataRow', dataRow);
-          return(
+          return(<div style={{textAlign: "left"}}>{
             dataRow.map((data, index) => {
               console.log('data', data.weight/width);
               return (
@@ -65,11 +73,11 @@ const Treemap = (props) => {
                   key={`data-${row}-${index}`}
                   name={data.name}
                   width={data.weight/width}
-                  totalWidth={width}
                   value={data.value}
                 />
               );
-            })
+            })}
+            </div>
         )})
       }
     </div>
